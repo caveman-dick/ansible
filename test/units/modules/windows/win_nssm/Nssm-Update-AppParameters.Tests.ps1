5e21abca2a8b028cdb73fbaf5af08e901398969b
Richard Mayes <caveman.dick@gmail.com>
2017-02-10 10:36:52 +0000

    Test that Nssm-Update-AppParameter throws when Nssm-Invoke fails

--- test/units/modules/windows/win_nssm/Nssm-Update-AppParameters.Tests.ps1
+++ test/units/modules/windows/win_nssm/Nssm-Update-AppParameters.Tests.ps1
@@ -7,11 +7,16 @@ Describe "Nssm-Update-AppParameters" {
     BeforeEach {
         $Global:LastExitCode = 0;
     }
-
+    
     It "calls Nssm-Invoke getting the AppParameters with the correct name" {
         Mock Nssm-Invoke {} -Verifiable -ParameterFilter { $cmd -eq "get `"SomeService`" AppParameters"}
         Mock Nssm-Invoke { return $cmd }
         Nssm-Update-AppParameters -name "SomeService" -appParameters "-name=value; -name2=value2"
         Assert-VerifiableMocks
-    }  
+    }
+
+    It "throws an error if fetching the AppParameters returns > 0" {
+        Mock Nssm-Invoke { $Global:LastExitCode = 1; return $cmd }
+        { Nssm-Update-AppParameters -name "SomeService" -appParameters "name=value name2=value2" } | Should Throw "Error updating AppParameters for service ""SomeService"""
+    }   
 }
